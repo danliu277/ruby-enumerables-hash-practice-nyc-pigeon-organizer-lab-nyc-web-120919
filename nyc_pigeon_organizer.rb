a70570b0fd2d5870c9b15b94eaf8062458c50907
Daniel Liu <danliu277@gmail.com>
2019-12-05 18:57:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,10 @@ def nyc_pigeon_organizer(data)
   attribute_index = 0
   result = {}
   while attribute_index < attributes_keys.length do
+    data_keys = data[attributes_keys[attribute_index]].keys
     data_index = 0
-    while data_index < attributes_keys[attribute_index][data_index].length do
-      puts "key", attributes_keys[attribute_index], data[attributes_keys[attribute_index]]
-      result[attributes_keys[attribute_index]] = attributes_keys[attribute_index][data_index]
+    while data_index < data_keys.length do
+      while 
       data_index += 1
     end
     attribute_index += 1
