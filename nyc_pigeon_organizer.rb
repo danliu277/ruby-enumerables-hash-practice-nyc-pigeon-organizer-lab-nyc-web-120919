6e900665e013686ac10c142a3c11614473ad778a
Daniel Liu <danliu277@gmail.com>
2019-12-05 19:17:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |key, attributes|
+    attributes.each do |value, names|
+      names.each do |name|
+        if !result[name]
+          result[name] = {}
+        end
+        if !result[name][key]
+          result[name][key] = []
+        end
+        result[name][key].push(value.to_s)
+      end
+    end
+  end
+  return result
 end
